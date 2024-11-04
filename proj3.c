59f346759bf7adfc2ab7961b0edec8bc568ec246
Bryanong9440 <bongzenh@terpmail.umd.edu>
2024-11-03 23:44:18 -0500

    Update proj3.c

--- proj3.c
+++ proj3.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//checking if someone has won
 int has_won(int board[][3]){
     for(int i = 0; i < 3; i++){
         if(board[i][0] == 1 && board[i][1] == 1 && board[i][2] == 1){
@@ -29,7 +29,7 @@ int has_won(int board[][3]){
         }
     return 0;
 }
-
+//Checking for valid moves and inserting X and O into board
 int move(int board[][3], int turn, int row, int column){
     if(row < 0 || row > 2 || column < 0 || column > 2){
         return -1;
@@ -41,6 +41,7 @@ int move(int board[][3], int turn, int row, int column){
         return 1;
     }
 }
+//Printing board after every move
 void print_board(int board[][3]) {
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
@@ -59,20 +60,26 @@ void print_board(int board[][3]) {
 int main()
 {
     printf("Welcome!\n");
+    //Creating an empty board
     int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
     int turn = 1;
+    //Loop of turns
     while (1){
         print_board(board);
+        //Getting and entering user input
         printf("Please enter a move: ");
         int row;
         int column;
         scanf("%i,%i", &row, &column);
+        //Making rows and columns align with array
         row --;
         column --;
+        //Checking validity
         if(move(board, turn, row, column) == -1){
             printf("Invalid input.\n");
             continue;
         }
+        //Checking if anyone won
         int result = has_won(board);
         if(result == 1){
             printf("X has won.");
@@ -81,7 +88,7 @@ int main()
             printf("Y has won.");
             break;
         }
+        //Switching turns
         turn *= -1;
     }
     return 0;
-}
