fff9550ed0dc3e3059015adc0cfb155cdd527e5d
Bryanong9440 <bongzenh@terpmail.umd.edu>
2024-11-07 18:51:07 -0500

    Update proj3.c

--- proj3.c
+++ proj3.c
@@ -1,67 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-//checking if someone has won
-int has_won(int board[][3]){
-    for(int i = 0; i < 3; i++){
-        if(board[i][0] == 1 && board[i][1] == 1 && board[i][2] == 1){
-            return 1;
-        }
-        if(board[i][0] == -1 && board[i][1] == -1 && board[i][2] == -1){
-            return -1;
-        }
-        if(board[0][i] == 1 && board[1][i] == 1 && board[2][i]== 1){
-            return 1;
-        }
-        if(board[0][i] == -1 && board[1][i] == -1 && board[2][i]== -1){
-            return -1;
-        }
-    }
-        if(board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1){
-            return 1;
-        }
-        if(board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1){
-            return 1;
-        }
-        if(board[0][0] == -1 && board[1][1] == -1 && board[2][2] == -1){
-            return -1;
-        }
-        if(board[2][0] == -1 && board[1][1] == -1 && board[0][2] == -1){
-            return -1;
-        }
-    return 0;
-}
-//Checking for valid moves and inserting X and O into board
-int move(int board[][3], int turn, int row, int column){
-    if(row < 0 || row > 2 || column < 0 || column > 2){
-        return -1;
-    }
-    if(board[row][column] != 0){
-        return -1;
-    }else{
-        board[row][column] = turn;
-        return 1;
-    }
-}
-//Printing board after every move
-void print_board(int board[][3]) {
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            if(board[i][j] == 1) {
-                printf("X");
-            }else if (board[i][j] == -1) {
-                printf("O");
-            }else{
-                printf("_");
-            }
-        }
-        printf("\n");
-    }
-}
-
-int main()
 {
     printf("Welcome!\n");
-    printf(">>> ");
     //Creating an empty board
     int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
     int turn = 1;
@@ -84,6 +22,7 @@ int main()
             while (getchar() != '\n'); 
             continue;
         }
+        printf(">>> ");
         int row;
         int column;
         //Getting the numbers for rows and columns
@@ -114,7 +53,7 @@ int main()
         //Draw check
         draw_check ++;
         if(draw_check == 9){
-            printf("It's a draw\n");
+            printf("It's a draw.\n");
             break;
         }
     }
